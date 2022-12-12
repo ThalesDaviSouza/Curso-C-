2643f9d2a59cf6ba490305c3526e0c60eac24caa
Thales Davi de Souza <81756816+ThalesDaviSouza@users.noreply.github.com>
2022-12-12 12:38:42 -0300

    Utilizando Interfaces

---
+++ Seção 14/Interfaces/Interfaces/Services/IPaymentService.cs
@@ -0,0 +1,6 @@
+﻿namespace Interfaces.Services {
+    internal interface IPaymentService {
+        public double Interest(double amount, int month);
+        public double PaymentFee(double amount);
+    }
+}
