75dab246a49d0ac610d0af2e481f84e345a453ef
Thales Davi de Souza <81756816+ThalesDaviSouza@users.noreply.github.com>
2022-12-16 11:15:53 -0300

    Testes com Extension Methods

---
+++ ExtensionMethods/ExtensionMethods/Extensions/StringExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿namespace System {
+    static class StringExtensionMethods {
+        public static string Cut(this string thisObj, int value) {
+            if (thisObj.Length <= value) {
+                return thisObj;
+            }
+
+            string cut = thisObj.Substring(0, value);
+
+            return $"{cut}...";
+        }
+    }
+}
